dd33875c7cc9b323b409fe0dff953139405d2328
naseriboroujeni <naseriboroujeni@gmail.com>
2024-09-02 15:43:18 +0300

    Instantiate ApolloServer

--- src/server.js
+++ src/server.js
@@ -35,3 +35,14 @@ const resolvers = {
       books: () => books,
    },
 };
+
+const server = new ApolloServer({
+   typeDefs,
+   resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+   listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
