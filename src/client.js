c2793dfa337882c8aa1b8d5a1d0da316a8ada166
naseriboroujeni <naseriboroujeni@gmail.com>
2024-09-03 15:43:19 +0300

    Add graphql client

--- src/client.js
+++ src/client.js
@@ -1,1 +1,15 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { gql, GraphQLClient } from 'graphql-request';
+
+const graphQLClient = new GraphQLClient('http://localhost:4000');
+
+const query = gql`
+   query GetBooks {
+      books {
+         title
+         author
+      }
+   }
+`;
+const results = await graphQLClient.request(query);
+
+console.log(results);
